56b9e079b3b74cb3113d5339271eacfd8b27e60e
FaYMan2 <suavarnavats244@gmail.com>
2024-08-04 22:40:57 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main
+      className="flex min-h-screen flex-col items-
+      center justify-auto"
+    >
+      <h1 className="bg-green-500 text-red-600 text-center">Hi mamma</h1>
+    </main>
+  );
+}
